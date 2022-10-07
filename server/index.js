182a68fab9eaa18e40adf4409f4d3c52dd225be7
michele.vigano <michele.vigano@gellify.com>
2022-10-07 14:56:21 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const Websocket = require('ws')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use('/', express.static(path.resolve(__dirname, '../client')))
+const server = app.listen(9876)
+
+const wss = new Websocket.Server({
+    server
+})
+
+wss.on('connection', function(ws) {
+    ws.on('message', function incoming(data,isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === Websocket.OPEN) {
+                client.send(data, { binary: isBinary })
+            }
+        })
+    })
+})
